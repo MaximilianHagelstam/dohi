92ff76c50d20672a519ea99ee7f678a11168573c
Maximilian Hagelstam <maximilian.hagelstam@gmail.com>
2022-01-12 14:59:48 +0200

    Bun that template

--- server/src/server.ts
+++
@@ -1,7 +0,0 @@
-import app from './app';
-import { PORT } from './utils/config';
-import logger from './utils/logger';
-
-app.listen(PORT, () => {
-  logger.info(`Server running in ${process.env.NODE_ENV} on port ${PORT}`);
-});
