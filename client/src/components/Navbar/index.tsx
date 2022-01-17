ba320f5ab0d0e439c9d6be8b5b7ffa9db3a59c53
Maximilian Hagelstam <maximilian.hagelstam@gmail.com>
2022-01-17 19:07:37 +0200

    Update navbar

--- client/src/components/Navbar/index.tsx
+++ client/src/components/Navbar/index.tsx
@@ -2,11 +2,17 @@ import { Button, Container, Nav, Navbar } from 'react-bootstrap';
 
 const MyNavbar = () => {
   return (
-    <Navbar bg="light" expand="lg">
-      <Container fluid>
-        <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
-        <Nav className="me-auto my-2 my-lg-0" />
-        <Button variant="outline-success">Login</Button>
+    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+      <Container>
+        <Navbar.Brand href="#home">Dohi</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto" />
+          <Nav>
+            <Nav.Link>Welcome James</Nav.Link>
+            <Button variant="outline-success">Sign In</Button>
+          </Nav>
+        </Navbar.Collapse>
       </Container>
     </Navbar>
   );
