76f852befefcb982c171fc523d11aec4de84fbbc
Maximilian Hagelstam <maximilian.hagelstam@gmail.com>
2022-01-18 09:14:44 +0200

    Design login component

---
+++ client/src/components/Login/index.tsx
@@ -0,0 +1,14 @@
+import { Button } from 'react-bootstrap';
+import './Login.css';
+
+const Login = () => {
+  return (
+    <div className="center">
+      <Button variant="light" size="lg">
+        Sign In With Google
+      </Button>
+    </div>
+  );
+};
+
+export default Login;
